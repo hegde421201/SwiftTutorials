009a56053df3d0f8ed803202234fcad79db66dee
ganesh.r.hegde <ganesh.r.hegde@ril.com>
2023-09-20 18:43:30 +0530

    update
    update

--- Package.swift
+++ Package.swift
@@ -4,12 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftTutorials",
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "SwiftTutorials",
-            path: "Sources"),
-    ]
+  name: "SwiftTutorials",
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "SwiftTutorials",
+      path: "Sources"),
+    .target(
+      name: "SwiftTutorials",
+      dependencies: [],
+      path: "Sources/main"
+    ),
+    .testTarget(
+      name: "SwiftTutorialsTests",  // Name of your test module
+      dependencies: ["SwiftTutorials"],  // Specify the target you want to test
+      path: "Tests/SwiftTutorialsTests"
+    ),
+  ]
 )
